e202ede3ddee379fd9faf57d68b43dae4f279c34
Victor Diaz <diazvaldiviav@gmail.com>
2023-03-06 23:09:08 -0500

    Terminando basic layout

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import NavBar from './NavBarLogo';
 import Header from './Header';
+import ProjectsSection from './ProjectsSection';
+import Footer from './Footer';
 import "./CSS/file.css";
 
 
@@ -10,6 +12,8 @@ root.render(
   <React.StrictMode>
     <NavBar></NavBar>
     <Header></Header>
+    <ProjectsSection></ProjectsSection>
+    <Footer></Footer>
   </React.StrictMode>
 );
 
